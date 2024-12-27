c5d8f4fba0540061a9f01b9c0b2c41136d1fb36f
Santhosh136 <snthosh602@gmail.com>
2024-12-27 12:52:14 +0530

    :zap: content from html template

--- src/main/java/rabbit/spring/mailer/features/email/EmailService.java
+++ src/main/java/rabbit/spring/mailer/features/email/EmailService.java
@@ -9,19 +9,26 @@ import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 record EmailType(String name, String subject, String templateFile) {}
 
 @Service
 public class EmailService {
   private Mailer mailer;
+  private SpringTemplateEngine templateEngine;
   private static Map<String, EmailType> emailTypes = new HashMap<>();
 
   static {
-    emailTypes.put("test", new EmailType("test", "Test subject", "email-templates/test.html"));
+    emailTypes.put("test", new EmailType(
+      "test", 
+      "Test subject", 
+      "email-templates/test.html"));
   }
 
-  EmailService() {
+  EmailService(SpringTemplateEngine templateEngine) {
+    this.templateEngine = templateEngine;
     this.mailer = MailerBuilder
       .withSMTPServerHost("localhost")
       .withSMTPServerPort(1025)
@@ -39,13 +46,25 @@ public class EmailService {
     return email;
   }
 
+  private String buildHtmlContent(String templateFile, String toAddress) {
+
+    String fullName = toAddress.replaceAll("((@.*)|[^a-zA-Z])+", " ").trim();
+
+    Context context = new Context();
+    context.setVariable("fullName", fullName);
+    String content = this.templateEngine.process(templateFile, context);
+    return content;
+  }
+
   public void sendMail(String toAddress, String fromAddress, String type) {
     try {
       EmailType emailType = emailTypes.get(type);
       String subject = emailType.subject();
-      String content = "<html><p> hello there! </p></html>";
+      String templateFilePath = emailType.templateFile();
 
-      Email email = this.buildEmail(toAddress, fromAddress, subject, content);
+      String htmlContent = this.buildHtmlContent(templateFilePath, toAddress);
+
+      Email email = this.buildEmail(toAddress, fromAddress, subject, htmlContent);
 
       Boolean isValidEmail = this.mailer.validate(email);
       if (!isValidEmail) {
@@ -54,7 +73,7 @@ public class EmailService {
       }
 
       this.mailer.sendMail(email);
-      
+
     } catch (MailException e) {
       System.err.println(e);
     }
